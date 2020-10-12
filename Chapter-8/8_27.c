bc71b7c2dc871f5a741a74f379c268ee592f9fdf
Kirill Arshanitsa <zatar@yandex.ru>
2020-10-12 13:26:22 +0300

    Fix 8_27

--- Chapter-8/8_27.c
+++ Chapter-8/8_27.c
@@ -2,34 +2,42 @@
 #include<string.h>
 
 double atof(const char*);
-int charToInt(const char);
+unsigned int charToInt(const char);
 
-int main(void){
-    printf("%f\n", atof("1234.123"));
+int main(void) {
+    printf("%f\n", atof("-1234.123"));
     return 0;
 }
-int charToInt(const char s){
-    for(int ch = 48, i = 0; i <= 9 ;ch++, i++){
+unsigned int charToInt(const char s) {
+    for (int ch = 48, i = 0; i <= 9; ch++, i++) {
         if (s == ch)
             return i;
     }
     return -1;
 }
 
-double atof(const char *nPtr){
+double atof(const char* nPtr) {
     double result = 0;
     unsigned int strSize = strlen(nPtr);
     unsigned int doublePartPtr = strcspn(nPtr, ".") + 1; // +1 or -1 for .
-    unsigned int intPartPtr = strcspn(nPtr, ".") - 1;
-    printf("%u\n",intPartPtr);
-    for(double doublePart = 0.1; doublePartPtr < strSize ;++doublePartPtr,  doublePart *= 0.1){
+    int intPartPtr = strcspn(nPtr, ".") - 1;
+
+    for (double doublePart = 0.1; doublePartPtr < strSize; ++doublePartPtr, doublePart *= 0.1) {
         //printf("%f %c %f\n",doublePart, nPtr[intPartPtr], doublePart * charToInt(nPtr[intPartPtr]));
         result += doublePart * charToInt(nPtr[doublePartPtr]);
     }
-    printf("%f\n",result );
-    for (int intPart = 1 ; intPartPtr >= 0; intPartPtr--, intPart *= 10){
-        //printf("%u %c %f\n",intPartPtr, nPtr[intPartPtr], result + intPart * charToInt(nPtr[intPartPtr]));
-        result += intPart * charToInt(nPtr[intPartPtr]);
+    if (nPtr[0] != '-') {
+        for (double intPart = 1; intPartPtr >= 0; intPartPtr--, intPart *= 10) {
+            //printf("%d %c %f\n",intPartPtr, nPtr[intPartPtr], result += intPart * charToInt(nPtr[intPartPtr]));
+            result += intPart * charToInt(nPtr[intPartPtr]);
+        }
+    }
+    else {
+        for (double intPart = 1; intPartPtr >= 1; intPartPtr--, intPart *= 10) {
+            //printf("%d %c %f\n",intPartPtr, nPtr[intPartPtr], result += intPart * charToInt(nPtr[intPartPtr]));
+            result += intPart * charToInt(nPtr[intPartPtr]);
+        }
+        result *= -1;
     }
 
     return result;
