b6c78577c6891053d07cea526d369302ca62c18c
Kirill Arshanitsa <zatar@yandex.ru>
2020-10-12 15:37:34 +0300

    Fix 8_27

--- Chapter-8/8_27.c
+++ Chapter-8/8_27.c
@@ -1,13 +1,66 @@
 #include<stdio.h>
+//#include<stdlib.h>
 #include<string.h>
 
 double atof(const char*);
 unsigned int charToInt(const char);
+int atoi(const char*);
+long int atol(const char*);
+double strtod(const char*, char **);
 
 int main(void) {
-    printf("%f\n", atof("-1234.123"));
+    char* strPtr;
+    //printf("%f\n", atof("-1234.123"));
+    //printf("%d\n", atoi("1230123"));
+    //printf("%ld\n", atol("-1000000010"));
+    printf("%f %p %s\n", strtod("52.1% are ammitted.", &strPtr), strPtr, strPtr);
     return 0;
 }
+
+double strtod(const char* nPtr, char** strPtr){
+    int beginStrPtr;
+    
+    for (int i = 0; nPtr[i] != '\0'; i++) {
+        if (nPtr[i] == '.') {
+            if (nPtr[i + 1] != '\0') {
+                strPtr = &nPtr[i + 1];
+            }
+        }
+    }
+ }
+
+long int atol(const char* nPtr) {
+    long int result = 0;
+    long int strSize = strlen(nPtr) - 1;
+    if (nPtr[0] == '-') {
+        for (long int intPart = 1; strSize >= 1; strSize--, intPart *= 10) {
+            result += intPart * charToInt(nPtr[strSize]);
+        }
+    }
+    else {
+        for (long int intPart = 1; strSize >= 0; strSize--, intPart *= 10) {
+            result += intPart * charToInt(nPtr[strSize]);
+        }
+    }
+    return result;
+}
+
+int atoi(const char* nPtr) {
+    int result = 0;
+    int strSize = strlen(nPtr) - 1;
+    if (nPtr[0] == '-') {
+        for (int intPart = 1; strSize >= 1; strSize--, intPart *= 10) {
+            result += intPart * charToInt(nPtr[strSize]);
+        }
+    }
+    else {
+        for ( int intPart = 1; strSize >= 0; strSize--, intPart *= 10) {
+            result += intPart * charToInt(nPtr[strSize]);
+        }
+    }
+    return result;
+}
+
 unsigned int charToInt(const char s) {
     for (int ch = 48, i = 0; i <= 9; ch++, i++) {
         if (s == ch)
