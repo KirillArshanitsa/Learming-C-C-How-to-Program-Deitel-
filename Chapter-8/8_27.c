1d9da022ba78d88b25b0ca1d628ebc7b37eb1655
Kirill Arshanitsa <zatar@yandex.ru>
2020-10-17 03:05:18 +0300

    Add func in 8_27

--- Chapter-8/8_27.c
+++ Chapter-8/8_27.c
@@ -1,66 +1,94 @@
 #include<stdio.h>
 //#include<stdlib.h>
 #include<string.h>
+#include<ctype.h>
 
 double atof(const char*);
 unsigned int charToInt(const char);
 int atoi(const char*);
 long int atol(const char*);
-double strtod(const char*, char **);
+double strtod(const char*, const char **);
+long strtol(const char *, char **, int);
+int checkStrIsDigit(const char*);
 
 int main(void) {
-    char* strPtr;
-    //printf("%f\n", atof("-1234.123"));
-    //printf("%d\n", atoi("1230123"));
-    //printf("%ld\n", atol("-1000000010"));
-    printf("%f %p %s\n", strtod("52.1% are ammitted.", &strPtr), strPtr, strPtr);
+    const char *strPtr = NULL;
+
+    printf("%f\n", atof("-1234.123"));
+    printf("%d\n", atoi("1230123"));
+    printf("%ld\n", atol("-1000000010"));
+    printf("%f %s\n", strtod("12.1234 are ammitted.", &strPtr), strPtr);
+
     return 0;
 }
 
-double strtod(const char* nPtr, char** strPtr){
-    int beginStrPtr;
-    
-    for (int i = 0; nPtr[i] != '\0'; i++) {
-        if (nPtr[i] == '.') {
-            if (nPtr[i + 1] != '\0') {
-                strPtr = &nPtr[i + 1];
-            }
+
+long strtol(const char* nPtr, char** endPtr, int base) {
+    return 0;
+}
+
+int checkStrIsDigit(const char *strPtr) {
+    for (size_t i = 0; strPtr[i] != '\0'; i++)
+        if (isdigit(strPtr[i] == 0)) {
+            printf("It's not a digigits - %s\n", strPtr);
+            return 0;
         }
-    }
- }
+    return 1;
+}
+
+double strtod(const char* nPtr, const char** endPtr) {
+    double result = NULL;
+    char strPart = NULL;
+    
+    sscanf_s(nPtr, "%lf%c", &result, &strPart);
+    if (strPart != NULL)
+        *endPtr = strchr(nPtr, strPart); 
+
+    return result;
+}
+
 
 long int atol(const char* nPtr) {
     long int result = 0;
-    long int strSize = strlen(nPtr) - 1;
-    if (nPtr[0] == '-') {
-        for (long int intPart = 1; strSize >= 1; strSize--, intPart *= 10) {
-            result += intPart * charToInt(nPtr[strSize]);
+    size_t strSize = strlen(nPtr) - 1;
+    if (strSize == 0)
+        printf("You enter empty string.\n");
+
+    if (checkStrIsDigit(nPtr)) {
+        if (nPtr[0] == '-') {
+            for (long int intPart = 1; strSize >= 1; strSize--, intPart *= 10) {
+                result += intPart * charToInt(nPtr[strSize]);
+            }
+        }
+        else {
+            for (long int intPart = 1; strSize >= 0; strSize--, intPart *= 10) {
+                result += intPart * charToInt(nPtr[strSize]);
+            }
         }
     }
-    else {
-        for (long int intPart = 1; strSize >= 0; strSize--, intPart *= 10) {
-            result += intPart * charToInt(nPtr[strSize]);
-        }
-    }
+
     return result;
 }
-
 int atoi(const char* nPtr) {
     int result = 0;
-    int strSize = strlen(nPtr) - 1;
-    if (nPtr[0] == '-') {
-        for (int intPart = 1; strSize >= 1; strSize--, intPart *= 10) {
-            result += intPart * charToInt(nPtr[strSize]);
+    size_t strSize = strlen(nPtr) - 1;
+    if (strSize == 0)
+        printf("You enter empty string.\n");
+    
+    if (checkStrIsDigit(nPtr)) {
+        if (nPtr[0] == '-') {
+            for (int intPart = 1; strSize >= 1; strSize--, intPart *= 10) {
+                result += intPart * charToInt(nPtr[strSize]);
+            }
         }
-    }
-    else {
-        for ( int intPart = 1; strSize >= 0; strSize--, intPart *= 10) {
-            result += intPart * charToInt(nPtr[strSize]);
+        else {
+            for (int intPart = 1; strSize != 0; strSize--, intPart *= 10) {
+                result += intPart * charToInt(nPtr[strSize]);
+            }
         }
     }
     return result;
 }
-
 unsigned int charToInt(const char s) {
     for (int ch = 48, i = 0; i <= 9; ch++, i++) {
         if (s == ch)
@@ -68,19 +96,22 @@ unsigned int charToInt(const char s) {
     }
     return -1;
 }
-
 double atof(const char* nPtr) {
     double result = 0;
-    unsigned int strSize = strlen(nPtr);
-    unsigned int doublePartPtr = strcspn(nPtr, ".") + 1; // +1 or -1 for .
-    int intPartPtr = strcspn(nPtr, ".") - 1;
+    size_t strSize = strlen(nPtr);
+
+    if (strSize == 0)
+        printf("You enter empty string.\n");
+
+    size_t doublePartPtr = strcspn(nPtr, ".") + 1; // +1 or -1 for .
+    size_t intPartPtr = strcspn(nPtr, ".") - 1;
 
     for (double doublePart = 0.1; doublePartPtr < strSize; ++doublePartPtr, doublePart *= 0.1) {
         //printf("%f %c %f\n",doublePart, nPtr[intPartPtr], doublePart * charToInt(nPtr[intPartPtr]));
         result += doublePart * charToInt(nPtr[doublePartPtr]);
     }
     if (nPtr[0] != '-') {
-        for (double intPart = 1; intPartPtr >= 0; intPartPtr--, intPart *= 10) {
+        for (double intPart = 1; intPartPtr != 0; intPartPtr--, intPart *= 10) {
             //printf("%d %c %f\n",intPartPtr, nPtr[intPartPtr], result += intPart * charToInt(nPtr[intPartPtr]));
             result += intPart * charToInt(nPtr[intPartPtr]);
         }
@@ -92,6 +123,5 @@ double atof(const char* nPtr) {
         }
         result *= -1;
     }
-
     return result;
 }
