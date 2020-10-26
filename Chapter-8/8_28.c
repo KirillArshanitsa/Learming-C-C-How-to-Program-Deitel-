c3bf5d00ecc29ee22f79553526c5fae2c264d942
user <user@user>
2020-10-26 11:39:40 +0300

    Fix 8_28

--- Chapter-8/8_28.c
+++ Chapter-8/8_28.c
@@ -4,30 +4,34 @@
 char *myStrcopy(char *, const char *);
 char *myStrncopy(char *, const char *, size_t);
 char *myStrcopyPtr(char *, const char *);
+char *myStrncopyPtr(char *, const char *, size_t);
 
 int main(void){
     char firstStr[21] = "FirstStr";
     char secondStr[7] = "Second";
-    //puts(myStrcopy(firstStr, secondStr));
-    //puts(myStrncopy(firstStr, secondStr, 3));
+    puts(myStrcopy(firstStr, secondStr));
+    puts(myStrncopy(firstStr, secondStr, 3));
     puts(myStrcopyPtr(firstStr, secondStr));
+    puts(myStrncopyPtr(firstStr, secondStr, 4));
     return 0;
 }
 
-char *myStrcopyPtr(char *s1, const char *s2) {
-    size_t s1Size = strlen(s1);
-    size_t s2Size = strlen(s2);
-    printf("%p\n",s1);
-    s1 += s1Size;
-    printf("%p\n",s1);
-    printf("4 - %c\n", *s1);
+char *myStrncopyPtr(char *s1, const char *s2, size_t n) {
+    char *biginChar = s1;
+    for ( s1 += n; n > 0; s1++, s2++, n-- )
+        *s1 = *s2;
+    *(s1 + 1) = '\0';
+    return biginChar;
+}
 
-    for ( s1 += s1Size; *s2 != '\0'; s1++, s2++ ){
-       //*s1 = *s2;
-       printf("%c %c\n", *s1, *s2);
-    }
-    //printf("size = %lu\n", strlen(s1 - s2Size - s1Size ));
-    return 0;
+char *myStrcopyPtr(char *s1, const char *s2) {
+    char *biginChar = s1;
+    size_t s1Size = strlen(s1);
+    for ( s1 += s1Size; *s2 != '\0'; s1++, s2++ )
+       *s1 = *s2;
+
+    *(s1 + 1) = '\0';
+    return biginChar;
 }
 
 char *myStrcopy(char *s1, const char *s2){
