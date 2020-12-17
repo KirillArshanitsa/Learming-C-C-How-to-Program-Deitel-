7c770218b15f7af3189c3e35dcfe471bcee4f3e4
Kirill Arshanitsa <zatar@yandex.ru>
2020-12-17 18:49:26 +0300

    Fix 8.38 v1

--- Chapter-8/8_38.c
+++ Chapter-8/8_38.c
@@ -6,12 +6,14 @@
 #define MAX_STRINGNUM_SIZE 100
 
 const char* parseChar(const char*);
-const char* getStrDecimalDegree(unsigned long int);
+const char* getStrDecimalDegree(unsigned int);
 const char* getStrDoubleDegree(unsigned long int);
+unsigned int getNumSize(unsigned long int);
+unsigned long int parseNum(unsigned long int, char *);
 
 int main(void)
 {
-    char num[MAX_NUM_SIZE] = "100121";
+    char num[MAX_NUM_SIZE] = "121120";
     puts("Enter number:");
     //gets(num);
     printf("You enter - %s\n", num);
@@ -21,15 +23,21 @@ int main(void)
     return 0;
 }
 
+unsigned int getNumSize(unsigned long int num) {
+    unsigned long int sizeNum;
+    for (sizeNum = 0; num != 0; sizeNum++) {
+        num /= 10;
+    }
+    return sizeNum;
+}
 
 
-const char * getStrDoubleDegree(unsigned long int num)
+const char* getStrDoubleDegree(unsigned long int num)
 {
-    static char str[MAX_STRINGNUM_SIZE];
-    unsigned long int quotient = 1;
+    static char strNum[MAX_STRINGNUM_SIZE];
     switch (num) {
-        case 0:
-            return "zero";
+            //case 0:
+            //    return "zero";
         case 1:
             return "one";
         case 2:
@@ -40,58 +48,30 @@ const char * getStrDoubleDegree(unsigned long int num)
             return "ten";
         case 11:
             return "eleven";
+        case 12:
+            return "twelve";
         case 20:
             return "twenty";
 
         default:
-            //return strcat(getStrDoubleDegree((num / 10) * 10), getStrDoubleDegree(num - (num / 10) * 10));
-            for (unsigned long int sizeNum = num; sizeNum != 0; sizeNum /= 10)
-                quotient *= 10;
-            while (quotient > 10) {
-                quotient /= 10;
-                printf("s %lu\n", quotient);
-                printf("num %lu\n", num);
-                if (quotient >= 100) {
-                    if (getStrDecimalDegree(quotient) == "-1") {
-                        quotient /= 10;
-                        printf("getStrDecimalDegree %lu\n", quotient);
-                        unsigned long int partNum = num / quotient;
-                        strcat_s(str, getStrDoubleDegree(partNum));
-                        //gеt hundred thousand millon billion
-                        strcat_s(str, " ");
-                        puts(str);
-                        strcat_s(str, getStrDecimalDegree(quotient));
-                        strcat_s(str, " ");
-                        puts(str);
-                        printf("getStrDecimalDegree quotient %lu\n", quotient);
-                        printf("getStrDecimalDegree num %lu\n", num);
-      
-                        num = num - (quotient * partNum);
-                        printf("getStrDecimalDegree num -= quotient %lu\n", num);
-                    }
-                    else {
-                        //get one two for hundred thousand millon billion
-                        unsigned long int bigPart = num / quotient;
-                        printf("bigPart %lu\n", bigPart);
-                        strcat_s(str, getStrDoubleDegree(bigPart));
-                        //gеt hundred thousand millon billion
-                        strcat_s(str, " ");
-                        strcat_s(str, getStrDecimalDegree(quotient));
-                        strcat_s(str, " ");
-                        num -= quotient;
-                    }
-                }
-                else {
-                    strcat_s(str, getStrDoubleDegree((num / quotient) * quotient));
-                    strcat_s(str, " ");
-                }
-            }
-            strcat_s(str, getStrDoubleDegree(num - (num / quotient) * quotient)); 
-            return str;
-        }
+            printf("Error in num %lu\n", num);
+            printf("strNum - %s\n", strNum);
+            //TODO change it
+            size_t strNumSize = strlen(strNum);
+            for (size_t a = 0; a < strNumSize; a++)
+                strNum[a] = 0;
+
+            strcat_s(strNum, getStrDoubleDegree((num / 10) * 10));
+            strcat_s(strNum, " ");
+            strcat_s(strNum, getStrDoubleDegree(num - (num / 10) * 10));
+            printf("strNum - %s\n", strNum);
+
+            return strNum;
+    }
 }
 
-const char* getStrDecimalDegree(unsigned long int num)
+
+const char* getStrDecimalDegree(unsigned int num)
 {
     switch (num) {
         case 100:
@@ -106,16 +86,93 @@ const char* getStrDecimalDegree(unsigned long int num)
             printf("Error in getStrDecimalDegree %d\n", num);
             return "-1";
     }
+
 }
+
+
+unsigned long int parseNum(unsigned long int num, char* resultPtr) {
+    unsigned int divider;
+    unsigned long int partOFNum;
+    unsigned long int remainderDivision = 0;
+
+    // TODO change it to list
+    if (num / 1000000000 > 0) {
+        divider = 1000000000;
+    }
+    else if (num / 1000000 > 0) {
+        divider = 1000000;
+    }
+    else if (num / 1000 > 0) {
+        divider = 1000;
+    }
+    else if (num / 100 > 0) {
+        divider = 100;
+    }
+    else {
+        divider = 1;
+    }
+
+    partOFNum = num / divider;
+
+    printf("divider - %lu\n", divider);
+    printf("partOFNum - %lu\n", partOFNum);
+
+    if (partOFNum < 100) {
+        strcat_s(resultPtr, MAX_STRINGNUM_SIZE, getStrDoubleDegree(partOFNum));
+        strcat_s(resultPtr, MAX_STRINGNUM_SIZE, " ");
+        strcat_s(resultPtr, MAX_STRINGNUM_SIZE, getStrDecimalDegree(divider));
+        strcat_s(resultPtr, MAX_STRINGNUM_SIZE, " ");
+        return num - partOFNum * divider;
+    }
+    else if ((partOFNum >= 100) & (divider >= 100)){
+
+        remainderDivision = parseNum(partOFNum, resultPtr);
+
+        printf("remainderDivision - %lu\n", remainderDivision);
+
+        if (remainderDivision) {
+            strcat_s(resultPtr, MAX_STRINGNUM_SIZE, getStrDoubleDegree(remainderDivision));
+        }
+        strcat_s(resultPtr, MAX_STRINGNUM_SIZE, " ");
+        strcat_s(resultPtr, MAX_STRINGNUM_SIZE, getStrDecimalDegree(divider));
+        strcat_s(resultPtr, MAX_STRINGNUM_SIZE, " ");
+        return num - partOFNum * divider;
+    }
+    else {
+        parseNum(num - partOFNum * divider, resultPtr);
+    }
+
+
+}
+
 
 const char* parseChar(const char * strPtr)
 {
     unsigned long int num;
+    static char result[MAX_STRINGNUM_SIZE];
+
     char* pointPtr = strchr((char *)strPtr, '.');
     size_t sizeNum = strlen(strPtr);
+
     if (pointPtr == NULL) {
         num = atol(strPtr);
-        return getStrDoubleDegree(num);
+        while (num != 0) {
+            sizeNum = getNumSize(num);
+            if (sizeNum > 2) {
+                num = parseNum(num, result);
+            }
+            else {
+                strcat_s(result, getStrDoubleDegree(num));
+                num = 0;
+            }
+        }
     }
+
+    //TODO
+    else {
+        ;
+    }
+
+    return result;
 }
 
