6b3a3177cd6e54e120454e9a414aad06edab2fab
user <user@user>
2021-01-23 09:32:49 +0300

    Fix 10_6

--- Chapter-10/10_6.c
+++ Chapter-10/10_6.c
@@ -1,38 +1,40 @@
 #include <stdio.h>
 
 struct customer{
-	char lastName[15];
-	char firstName[15];
-	int customerNumber;
-	struct {
-		char phoneNumber[11];
-		char address[50];
-		char city[15];
-		char state[3];
-		char zipCode[6];
-	} personal;
-} customerRecord, *customerPtr;
+    char lastName[15];
+    char firstName[15];
+    int customerNumber;
+    struct {
+        char phoneNumber[11];
+        char address[50];
+        char city[15];
+        char state[3];
+        char zipCode[6];
+    } personal;
+} customerRecord = {"Kirill", "A", 4, {"123-456", "3", "SPB",
+                                       "N/A", "192283"}}, *customerPtr;
 
 int main(void) {
-	customerPtr = &customerRecord;
 
-	printf("%s\n", customerRecord.lastName);
-	printf("%s\n", customerPtr -> lastName);
-	printf("%s\n", customerRecord.firstName);
-	printf("%s\n", customerPtr -> firstName);
-	printf("%d\n", customerRecord.customerNumber);
-	printf("%d\n", customerPtr -> customerNumber);
-	printf("%s\n", customerRecord.personal.phoneNumber);
-	printf("%s\n", customerPtr -> personal.phoneNumber);
-	printf("%s\n", customerRecord.personal.address);
-	printf("%s\n", customerPtr -> personal.address);
-	printf("%s\n", customerRecord.personal.city);
-	printf("%s\n", customerPtr -> personal.city);
-	printf("%s\n", customerRecord.personal.state);
-	printf("%s\n", customerPtr -> personal.state);
-	printf("%s\n", customerRecord.personal.zipCode);
-	printf("%s\n", customerPtr -> personal.zipCode);
+    customerPtr = &customerRecord;
 
-	return 0;
+    printf("%s\n", customerRecord.lastName);
+    printf("%s\n", customerPtr -> lastName);
+    printf("%s\n", customerRecord.firstName);
+    printf("%s\n", customerPtr -> firstName);
+    printf("%d\n", customerRecord.customerNumber);
+    printf("%d\n", customerPtr -> customerNumber);
+    printf("%s\n", customerRecord.personal.phoneNumber);
+    printf("%s\n", customerPtr -> personal.phoneNumber);
+    printf("%s\n", customerRecord.personal.address);
+    printf("%s\n", customerPtr -> personal.address);
+    printf("%s\n", customerRecord.personal.city);
+    printf("%s\n", customerPtr -> personal.city);
+    printf("%s\n", customerRecord.personal.state);
+    printf("%s\n", customerPtr -> personal.state);
+    printf("%s\n", customerRecord.personal.zipCode);
+    printf("%s\n", customerPtr -> personal.zipCode);
+
+    return 0;
 }
 
