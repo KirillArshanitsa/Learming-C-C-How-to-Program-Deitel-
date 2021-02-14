5bad93c14a0c3b5b91c4527b5fe649538da2c8c1
Kirill Arshanitsa <zatar@yandex.ru>
2021-02-14 16:08:52 +0300

    Add 11_11

---
+++ Chapter-11/11_11.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <string.h>
+
+#define NAME_STR_SIZE 15
+#define AGE_STR_SIZE 2
+
+struct person {
+	char lastName[NAME_STR_SIZE];
+	char firstName[NAME_STR_SIZE];
+	char age[AGE_STR_SIZE];
+};
+
+void a(const char[]);
+void b(const char[]);
+void c(const char[], const char[], unsigned int);
+void readData(const char[]);
+
+int main(void)
+{
+	const char fileName[] = "nameage.dat";
+	a(fileName);
+	b(fileName);
+
+	puts("Read data");
+	readData(fileName);
+	return 0;
+}
+
+void c(const char fileName[], const char lastNameM[], unsigned int id)
+{
+	FILE* filePtr;
+	char str[55];
+	unsigned int fileId;
+	char lastName[NAME_STR_SIZE];
+	char firstName[NAME_STR_SIZE];
+	char age[AGE_STR_SIZE];
+	unsigned int currentId = 0;
+
+	fopen_s(&filePtr, fileName, "r+");
+	if (filePtr == NULL) {
+		printf("Error open file %s", fileName);
+		return;
+	}
+	while (!feof(filePtr)) {
+		
+		fscanf_s(filePtr, "%s %s %s", lastName, firstName, age);
+		if (currentId == id) {
+			if (strcmp(lastName, "unassigned") == 0)
+				puts("No info");
+			fprintf(filePtr, "%s %s %s", lastNameM, firstName, age);
+		}
+		currentId++;
+	}
+	fclose(filePtr);
+}
+
+void readData(const char fileName[]) 
+{
+	FILE *filePtr;
+	char str[55];
+	fopen_s(&filePtr, fileName, "r");
+	if (filePtr == NULL) {
+		printf("Error open file %s", fileName);
+		return;
+	}
+	while (!feof(filePtr)) {
+		if (fgets(str, 55, filePtr))
+			printf(str);
+	}
+	fclose(filePtr);
+
+}
+
+void a(const char fileName[]) 
+{
+	FILE* filePtr;
+	struct person tmpPerson = { "unassigned", "","0" };
+	fopen_s(&filePtr, fileName, "w");
+
+	if (filePtr == NULL) {
+		puts("Error open file nameage.dat");
+		return;
+	}
+	for (int i = 0; i < 100; i++)
+		fprintf_s(filePtr, "%s %s %s\n", tmpPerson.lastName, tmpPerson.firstName, tmpPerson.age);
+	fclose(filePtr);
+}
+
+
+void b(const char fileName[]) 
+{
+	FILE* filePtr;
+	char lastName[NAME_STR_SIZE];
+	char firstName[NAME_STR_SIZE];
+	char age[AGE_STR_SIZE];
+
+	fopen_s(&filePtr, fileName, "r+");
+	if (filePtr == NULL) {
+		puts("Error open file nameage.dat");
+		return;
+	}
+	for (int i = 0; i < 10; i++) {
+		sprintf_s(lastName, "Userovich_%d", i);
+		sprintf_s(firstName, "User_%d", i);
+		sprintf_s(age, "%d", i);
+		fprintf_s(filePtr, "%s %s %s\n", lastName, firstName, age);
+	}
+
+	fclose(filePtr);
+}
