7388f4fa6edc9023aebdb55bf997535dc3372a2a
Kirill Arshanitsa <zatar@yandex.ru>
2021-08-12 23:58:07 +0300

    Fix 7_29

--- Chapter-7/7_29.c
+++ Chapter-7/7_29.c
@@ -130,15 +130,15 @@ void runCommands(int memory[], const unsigned int final) {
                 printString(memory, operand);
                 break;
             case 40:
-                instructionCounter = operand;
+                instructionCounter = operand - 1;
                 break;
             case 41:
                 if (accumulator < 0)
-                    instructionCounter = operand;
+                    instructionCounter = operand - 1;
                 break;
             case 42:
                 if (accumulator == 0)
-                    instructionCounter = operand;
+                    instructionCounter = operand - 1;
                 break;
             case 43:
                 return;
